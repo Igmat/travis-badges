b489bfdf8391f55918522331dca8b47e77d8ff44
Igor Chulinda <ichulinda@dataart.com>
2016-10-25 19:44:18 +0200

    Try to use node-travis-ci

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import * as express from 'express';
-const webshot = require('webshot');
-import * as request from 'request';
-import * as temp from 'temp';
-import * as fs from 'fs';
+const Travis = require('travis-ci');
+var travis = new Travis({
+    version: '2.0.0'
+});
 
 var app = express();
 declare var url;
@@ -11,107 +11,22 @@ app.use(express.static(__dirname + '/public'));
 //  /^\/(.+)/
 app.get('/repos/(*)', (req, res) => {
   var repo = req.params[0];
-  var html = '<br/><table><tr><th>Builds</th></tr>';
-  var options = {
-    url: 'https://api.travis-ci.org/repos/' + repo,
-    headers: {
-      'Accept': 'application/vnd.travis-ci.2+json'
-    }
-  };
-  request(options, (error, response, body) => {
-    if (error || response.statusCode !== 200) {
-      res.status(400);
-      url = req.url;
-      res.send('Branch build id not found');
-      return;
-    }
-
-    var buildId = JSON.parse(body).branch.id;
-    if (!buildId) {
-      res.status(400);
-      url = req.url;
-      res.send('Branch build id not found');
-    }
-    var options2 = {
-      url: 'https://api.travis-ci.org/builds/' + buildId,
-      headers: {
-        'Accept': 'application/vnd.travis-ci.2+json'
-      }
-    };
-    request(options2, (error2, response2, body2) => {
-      if (error2 || response2.statusCode !== 200) {
-        res.status(400);
-        url = req.url;
-        res.send('Error retrieving build');
-        return;
-      }
-      //console.log(body2);
-      var jobs = JSON.parse(body2).jobs;
-      if (!jobs) {
-        res.status(400);
-        url = req.url;
-        res.send('Jobs not found in build');
-      }
-      jobs.forEach(function (job) {
-        var state = job.state;
-        var number = job.number;
-        html += '<td>' + number + ' ';
-        if (state === 'passed') {
-          html += '<span style="color:green;">passed</span>';
-        } else if (state === 'failed') {
-          html += '<span style="color:red;">failed</span>';
-        } else {
-          html += state;
-        }
-        html += '</td></tr>';
+  travis.repos(repo).get((err, repoResponse) => {
+    let lastBuildId = repoResponse.repos[0].last_build_id;
+    travis.builds(lastBuildId).get((err, buildResponse) => {
+      let result = buildResponse.jobs.map((job) => {
+        return {
+          version: job.config.node_js,
+          status: job.state
+        };
       });
-      //console.log(html);
-      html += '</table>';
-      html += '<br/>Build ID: ' + buildId;
-      screenShot(html, (original, cleanupScreenShot) => {
-        writeFileToResponse(original, res, () => {
-          cleanupScreenShot();
-        });
-      });
+      res.status(200);
+      url = req.url;
+      res.send(result);
     });
-
   });
 });
 
 app.listen(app.get('port'), () => {
   console.log('Node app is running at localhost:' + app.get('port'));
 });
-
-function createTempPng() {
-  return temp.path({ suffix: '.png' });
-}
-
-function writeFileToResponse(file: string, resp, callback) {
-  resp.writeHead(200, { 'Content-Type': 'image/png' });
-  fs.readFile(file, (err, data) => {
-    resp.end(data);
-    callback();
-  });
-}
-
-function cleanupTempFile(file: string) {
-  fs.unlink(file, (err) => {
-    if (err) console.error(err);
-  });
-}
-
-function screenShot(html, callback) {
-  var original = createTempPng();
-  var options = {
-    shotSize: {
-      width: 320,
-      height: 'all'
-    },
-    siteType: 'html'
-  };
-  webshot(html, original, options, (err) => {
-    callback(original, () => {
-      cleanupTempFile(original);
-    });
-  });
-}
