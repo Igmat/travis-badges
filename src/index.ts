7ab2a018c49d8729cf9d884df0d38a9c7c42c0fa
Igor Chulinda <ichulinda@dataart.com>
2016-10-26 13:01:59 +0200

    Generating badges with `gh-badges`

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,46 @@
 import * as express from 'express';
 const Travis = require('travis-ci');
 var travis = new Travis({
-    version: '2.0.0'
+  version: '2.0.0'
 });
+const badge = require('gh-badges');
 
 var app = express();
-declare var url;
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 //  /^\/(.+)/
 app.get('/repos/(*)', (req, res) => {
   var repo = req.params[0];
   travis.repos(repo).get((err, repoResponse) => {
-    let lastBuildId = repoResponse.repos[0].last_build_id;
-    travis.builds(lastBuildId).get((err, buildResponse) => {
-      let result = buildResponse.jobs.map((job) => {
-        return {
-          version: job.config.node_js,
-          status: job.state
-        };
-      });
+    let lastBuildId = repoResponse.branch.id;
+    travis.builds(lastBuildId).get(async (err, buildResponse) => {
+      console.log(buildResponse);
+      let result = await Promise.all((buildResponse.jobs as Array<any>).map((job) => {
+        let colorscheme: 'brightgreen' | 'green' | 'yellow' | 'yellowgreen' | 'orange' | 'red' | 'blue' | 'grey' | 'gray' | 'lightgrey' | 'lightgray';
+        switch (job.state) {
+          case 'passed':
+            colorscheme = 'green';
+            break;
+          case 'started':
+            colorscheme = 'yellow';
+            break;
+          case 'failed':
+            colorscheme = 'red';
+            break;
+          default:
+            colorscheme = 'gray';
+            break;
+        }
+        return new Promise((resolve: (value: string) => void) => {
+          badge({ text: ['node v' + job.config.node_js, job.state], colorscheme, template: 'flat' },
+            (svg, err) => {
+              resolve(svg);
+              // svg is a String of your badge.
+            });
+        });
+      }));
       res.status(200);
-      url = req.url;
-      res.send(result);
+      res.send(result.join(''));
     });
   });
 });
